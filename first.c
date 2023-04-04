95bea0753a582322522d818b1359e28f81cfa1f1
Rakib-programmer-23 <rakibroni374@gmail.com>
2023-04-04 22:49:39 +0600

    commit all

---
+++ first.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+     printf("Recently I heared that you have achieved 95% marks in you exam.\nThis is brilliant!\nI wish you'll shine in your life!\tGood luck with all the barriers(^)in\nyour life.");
+     return 0;
+
+}
